a9383fa275ffbd130b479ba1869dd7816714fd00
Robin Lee <robin051@gmail.com>
2014-01-20 22:18:27 +0800

    Added mind map

---
+++ Printed Solar Cells on Building.mm
@@ -0,0 +1,77 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Printed Solar Cells on Building" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1390182097044"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<node TEXT="Solar Cell" POSITION="right" ID="ID_409239901" CREATED="1390182100578" MODIFIED="1390182136751">
+<edge COLOR="#ff0000"/>
+<node TEXT="Efficiency" ID="ID_451812285" CREATED="1390182136970" MODIFIED="1390182139718"/>
+<node TEXT="Cost" ID="ID_797060489" CREATED="1390182140276" MODIFIED="1390182141506">
+<node TEXT="Scale of manufacturing" ID="ID_1593960206" CREATED="1390182158934" MODIFIED="1390182163729"/>
+<node TEXT="Return on investment" ID="ID_551705984" CREATED="1390182169863" MODIFIED="1390182176998"/>
+</node>
+</node>
+<node TEXT="Effective Power Source" POSITION="left" ID="ID_1876810144" CREATED="1390182193448" MODIFIED="1390182227092">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Solar Irradiance at a particular place" ID="ID_200493661" CREATED="1390182227360" MODIFIED="1390182249904"/>
+<node TEXT="Building facade surface area" ID="ID_661292213" CREATED="1390182250415" MODIFIED="1390182286884">
+<node TEXT="Whole coverage" ID="ID_1672427276" CREATED="1390182374150" MODIFIED="1390182381229"/>
+<node TEXT="Partial coverage" ID="ID_1446494609" CREATED="1390182382329" MODIFIED="1390182384806"/>
+<node TEXT="Roof top" ID="ID_1499735212" CREATED="1390182385130" MODIFIED="1390182387558"/>
+</node>
+<node TEXT="Market size (total facade surface area)" ID="ID_1200683656" CREATED="1390182288159" MODIFIED="1390182326857"/>
+</node>
+<node TEXT="Environmental benefits" POSITION="right" ID="ID_833878648" CREATED="1390182423101" MODIFIED="1390182431447">
+<edge COLOR="#00ffff"/>
+<node TEXT="Current electricity consumption" ID="ID_1609127301" CREATED="1390182431644" MODIFIED="1390182451294"/>
+<node TEXT="Percentage of commercial consumption &amp; associated carbon emission" ID="ID_495883733" CREATED="1390182451676" MODIFIED="1390182478243"/>
+</node>
+</node>
+</map>
